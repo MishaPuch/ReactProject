db2af3fe0449d3e820e72ffc84111ae80404a4bb
MishaPuch <mpuczkowskij@gmail.com>
2023-06-18 08:00:52 +0200

    start of the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header className='header'>
+            <img src='https://freepngimg.com/thumb/duck/8-duck-png-image-thumb.png'/>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+function NavBar(){
+    return(
+        <nav className='nav'>
+            <div>
+            <div>
+                <a href='#'>Profile</a></div>
+            <div>
+                <a href='#'>M</a></div>
+            <div>
+                <a href='#'>News</a></div>
+            <div>
+                <a href='#'>Music</a></div>
+            <div>
+                <a href='#'>Setings</a></div>          
+            </div>
+        
+      </nav>
+    )
+}
+export default NavBar
